789fbd9c438debd13c050fe54b71b665b1f22b81
haikal-spec <muhammadhaikal140404@gmail.com>
2025-05-29 15:46:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Set the app title
-st.title('My First Streamlit App !!')
+st.title('YOWAIMOOO !!')
 
 # Add a welcome message
 st.write('Domain Expansion!')
